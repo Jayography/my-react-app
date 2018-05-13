5a7e0728907e671f6627c3302a1453023ad71133
Jayography <32205060+Jayography@users.noreply.github.com>
2018-05-13 08:06:32 -0400

    Adding the React App

---
+++ my-app/src/Cards.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import Toggle from 'material-ui/Toggle';
+import avatarImage from './images/Image-2.jpg';
+import sanfran from './images/sanfran.png'
+export default class CardExampleControlled extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+    };
+  }
+
+  handleExpandChange = (expanded) => {
+    this.setState({expanded: expanded});
+  };
+
+  handleToggle = (event, toggle) => {
+    this.setState({expanded: toggle});
+  };
+
+  handleExpand = () => {
+    this.setState({expanded: true});
+  };
+
+  handleReduce = () => {
+    this.setState({expanded: false});
+  };
+
+  render() {
+    return (
+      <Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
+        <CardHeader
+          title="Juwan Johnson"
+          subtitle="Multimedia Content Producer"
+          avatar={avatarImage}
+          actAsExpander={true}
+          showExpandableButton={true}
+        />
+        <CardText>
+          <Toggle
+            toggled={this.state.expanded}
+            onToggle={this.handleToggle}
+            labelPosition="right"
+            label="This toggle controls the expanded state of the component."
+          />
+        </CardText>
+        <CardMedia
+          expandable={true}
+          overlay={<CardTitle title="User Interface Designer" subtitle="UI Developer" />}
+        >
+          <img src={sanfran} alt="San Francisco Skyline" />
+        </CardMedia>
+        <CardTitle title="Card title" subtitle="Card subtitle" expandable={true} />
+        <CardText expandable={true}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+        <CardActions>
+          <FlatButton label="Expand" onClick={this.handleExpand} />
+          <FlatButton label="Reduce" onClick={this.handleReduce} />
+        </CardActions>
+      </Card>
+    );
+  }
+}
